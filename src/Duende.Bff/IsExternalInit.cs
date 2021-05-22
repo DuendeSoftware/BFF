01b9125b4fb2b5a28d4f0497c8b2c010d97878da
Brock Allen <brockallen@gmail.com>
2021-05-22 10:40:27 -0400

    add header

--- src/Duende.Bff/IsExternalInit.cs
+++ src/Duende.Bff/IsExternalInit.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
 namespace System.Runtime.CompilerServices
 {
     // Allows "init" properties in .NET Core 3.1.
