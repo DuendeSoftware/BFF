ede2d444ede51b5e3dea2963393561776fa6171d
Dominick Baier <dbaier@leastprivilege.com>
2021-06-19 09:31:10 +0200

    remove StructuredTransformer

--- src/Duende.Bff/Proxy/RequestUtilities.cs
+++ src/Duende.Bff/Proxy/RequestUtilities.cs
@@ -2,58 +2,15 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
 
 namespace Duende.Bff
 {
+    // todo: check with latest - should be public in YARP?
     internal static class RequestUtilities
     {
-        internal static bool ShouldSkipResponseHeader(string headerName, bool isHttp2OrGreater)
-        {
-            if (isHttp2OrGreater)
-            {
-                return _invalidH2H3ResponseHeaders.Contains(headerName);
-            }
-            else
-            {
-                return headerName.Equals(HeaderNames.TransferEncoding, StringComparison.OrdinalIgnoreCase);
-            }
-        }
-
-        private static readonly HashSet<string> _invalidH2H3ResponseHeaders = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-        {
-            HeaderNames.Connection,
-            HeaderNames.TransferEncoding,
-            HeaderNames.KeepAlive,
-            HeaderNames.Upgrade,
-            "Proxy-Connection"
-        };
-
-        /// <summary>
-        /// Appends the given path and query to the destination prefix while avoiding duplicate '/'.
-        /// </summary>
-        /// <param name="destinationPrefix">The scheme, host, port, and possibly path base for the destination server.</param>
-        /// <param name="path">The path to append.</param>
-        /// <param name="query">The query to append</param>
-        internal static Uri MakeDestinationAddress(string destinationPrefix, PathString path, QueryString query)
-        {
-            ReadOnlySpan<char> prefixSpan = destinationPrefix;
-
-            if (path.HasValue && destinationPrefix.EndsWith('/'))
-            {
-                // When PathString has a value it always starts with a '/'. Avoid double slashes when concatenating.
-                prefixSpan = prefixSpan[0..^1];
-            }
-
-            var targetAddress = string.Concat(prefixSpan, path.ToUriComponent(), query.ToUriComponent());
-
-            return new Uri(targetAddress, UriKind.Absolute);
-        }
-
         // Note: HttpClient.SendAsync will end up sending the union of
         // HttpRequestMessage.Headers and HttpRequestMessage.Content.Headers.
         // We don't really care where the proxied headers appear among those 2,
