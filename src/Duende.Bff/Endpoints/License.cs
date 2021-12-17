ee726e28195d92af69bf55eff9c2c5785e1f0035
Brock Allen <brockallen@gmail.com>
2021-12-17 12:19:45 -0500

    add updated licence logic for BFF edition

---
+++ src/Duende.Bff/Endpoints/License.cs
@@ -0,0 +1,202 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using System;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Duende.Bff.Endpoints
+{
+    internal class License
+    {
+        // for testing
+        internal License(params Claim[] claims)
+            : this(new ClaimsPrincipal(new ClaimsIdentity(claims)))
+        {
+        }
+
+        public License(ClaimsPrincipal claims)
+        {
+            CompanyName = claims.FindFirst("company_name")?.Value;
+            ContactInfo = claims.FindFirst("contact_info")?.Value;
+
+            if (Int64.TryParse(claims.FindFirst("exp")?.Value, out var exp))
+            {
+                var expDate = DateTimeOffset.FromUnixTimeSeconds(exp);
+                Expiration = expDate.UtcDateTime;
+            }
+
+            var edition = claims.FindFirst("edition")?.Value;
+            if (!Enum.TryParse<License.LicenseEdition>(edition, true, out var editionValue))
+            {
+                throw new Exception($"Invalid edition in licence: '{edition}'");
+            }
+
+            Edition = editionValue;
+            ISVFeature = claims.HasClaim("feature", "isv");
+
+            if (IsCommunityEdition && ISVFeature)
+            {
+                throw new Exception("Invalid License: ISV is not valid for community edition.");
+            }
+
+            if (IsBffEdition && ISVFeature)
+            {
+                throw new Exception("Invalid License: ISV is not valid for BFF edition.");
+            }
+
+            if (IsBffEdition)
+            {
+                // for BFF-only edition we set BFF flag and ignore all other features
+                BffFeature = true;
+                ClientLimit = 0;
+                IssuerLimit = 0;
+                return;
+            }
+
+            KeyManagementFeature = claims.HasClaim("feature", "key_management");
+            switch (Edition)
+            {
+                case LicenseEdition.Enterprise:
+                case LicenseEdition.Business:
+                case LicenseEdition.Community:
+                    KeyManagementFeature = true;
+                    break;
+            }
+
+            ResourceIsolationFeature = claims.HasClaim("feature", "resource_isolation");
+            switch (Edition)
+            {
+                case LicenseEdition.Enterprise:
+                case LicenseEdition.Community:
+                    ResourceIsolationFeature = true;
+                    break;
+            }
+
+            DynamicProvidersFeature = claims.HasClaim("feature", "dynamic_providers");
+            switch (Edition)
+            {
+                case LicenseEdition.Enterprise:
+                case LicenseEdition.Community:
+                    DynamicProvidersFeature = true;
+                    break;
+            }
+
+            BffFeature = claims.HasClaim("feature", "bff");
+            switch (Edition)
+            {
+                case LicenseEdition.Enterprise:
+                case LicenseEdition.Business:
+                case LicenseEdition.Community:
+                    BffFeature = true;
+                    break;
+            }
+
+
+            if (!claims.HasClaim("feature", "unlimited_clients"))
+            {
+                // default values
+                if (ISVFeature)
+                {
+                    // default for all ISV editions
+                    ClientLimit = 5;
+                }
+                else
+                {
+                    // defaults limits for non-ISV editions
+                    switch (Edition)
+                    {
+                        case LicenseEdition.Business:
+                            ClientLimit = 15;
+                            break;
+                        case LicenseEdition.Starter:
+                            ClientLimit = 5;
+                            break;
+                    }
+                }
+
+                if (Int32.TryParse(claims.FindFirst("client_limit")?.Value, out var clientLimit))
+                {
+                    // explicit, so use that value
+                    ClientLimit = clientLimit;
+                }
+
+                if (!ISVFeature)
+                {
+                    // these for the non-ISV editions that always have unlimited, regardless of explicit value
+                    switch (Edition)
+                    {
+                        case LicenseEdition.Enterprise:
+                        case LicenseEdition.Community:
+                            // unlimited
+                            ClientLimit = null;
+                            break;
+                    }
+                }
+            }
+
+            if (!claims.HasClaim("feature", "unlimited_issuers"))
+            {
+                // default 
+                IssuerLimit = 1;
+
+                if (Int32.TryParse(claims.FindFirst("issuer_limit")?.Value, out var issuerLimit))
+                {
+                    IssuerLimit = issuerLimit;
+                }
+
+                // these for the editions that always have unlimited, regardless of explicit value
+                switch (Edition)
+                {
+                    case LicenseEdition.Enterprise:
+                    case LicenseEdition.Community:
+                        // unlimited
+                        IssuerLimit = null;
+                        break;
+                }
+            }
+        }
+
+        public string CompanyName { get; set; }
+        public string ContactInfo { get; set; }
+
+        public DateTime? Expiration { get; set; }
+
+        public LicenseEdition Edition { get; set; }
+
+        internal bool IsEnterpriseEdition => Edition == LicenseEdition.Enterprise;
+        internal bool IsBusinessEdition => Edition == LicenseEdition.Business;
+        internal bool IsStarterEdition => Edition == LicenseEdition.Starter;
+        internal bool IsCommunityEdition => Edition == LicenseEdition.Community;
+        internal bool IsBffEdition => Edition == LicenseEdition.Bff;
+
+        public int? ClientLimit { get; set; }
+        public int? IssuerLimit { get; set; }
+
+        public bool KeyManagementFeature { get; set; }
+        public bool ResourceIsolationFeature { get; set; }
+        public bool DynamicProvidersFeature { get; set; }
+        public bool ISVFeature { get; set; }
+        public bool BffFeature { get; set; }
+
+        public enum LicenseEdition
+        {
+            Enterprise,
+            Business,
+            Starter,
+            Community,
+            Bff
+        }
+
+        public override string ToString()
+        {
+#if NET6_0_OR_GREATER
+            return JsonSerializer.Serialize(this, new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull });
+#else
+            return JsonSerializer.Serialize(this, new JsonSerializerOptions { IgnoreNullValues = true });
+#endif        
+        }
+    }
+}
--- src/Duende.Bff/Endpoints/LicenseValidator.cs
+++ src/Duende.Bff/Endpoints/LicenseValidator.cs
@@ -1,17 +1,15 @@
-// // Copyright (c) Duende Software. All rights reserved.
-// // See LICENSE in the project root for license information.
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
 
+
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Security.Claims;
 using System.Security.Cryptography;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.JsonWebTokens;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Duende.Bff.Endpoints
 {
@@ -20,13 +18,11 @@ namespace Duende.Bff.Endpoints
         const string LicenseFileName = "Duende_IdentityServer_License.key";
 
         static ILogger _logger;
-        static BffOptions _options;
         static License _license;
 
         public static void Initalize(ILoggerFactory loggerFactory, BffOptions options)
         {
             _logger = loggerFactory.CreateLogger("Duende.Bff");
-            _options = options;
 
             var key = options.LicenseKey ?? LoadFromFile();
             _license = ValidateKey(key);
@@ -58,21 +54,19 @@ namespace Duende.Bff.Endpoints
             }
             else
             {
-                if (_logger.IsEnabled(LogLevel.Debug))
-                {
-                    _logger.LogDebug("The validated licence key details: {@license}", _license);
-                }
+                Action<string, object[]> func = _license.ISVFeature ? _logger.LogTrace : _logger.LogDebug;
+                func.Invoke("The validated licence key details: {@license}", new[] { _license });
 
                 if (_license.Expiration.HasValue)
                 {
                     var diff = DateTime.UtcNow.Date.Subtract(_license.Expiration.Value.Date).TotalDays;
-                    if (diff > 0 && !_license.ISV)
+                    if (diff > 0 && !_license.ISVFeature)
                     {
                         errors.Add($"Your license for Duende IdentityServer expired {diff} days ago.");
                     }
                 }
 
-                if (_license.IsStarter && _license.BFF == false)
+                if (!_license.BffFeature)
                 {
                     errors.Add($"Your IdentityServer license does not include the BFF feature.");
                 }
@@ -96,15 +90,16 @@ namespace Duende.Bff.Endpoints
             {
                 if (_license.Expiration.HasValue)
                 {
-                    _logger.LogInformation(
-                        "You have a valid license key for Duende IdentityServer {edition} edition for use at {licenseCompany}. The license expires on {licenseExpiration}.",
-                        _license.Edition, _license.CompanyName, _license.Expiration.Value.ToLongDateString());
+                    Action<string, object[]> log = _license.ISVFeature ? _logger.LogTrace : _logger.LogInformation;
+                    log.Invoke("You have a valid license key for Duende IdentityServer {edition} edition for use at {licenseCompany}. The license expires on {licenseExpiration}.",
+                        new object[] { _license.Edition, _license.CompanyName, _license.Expiration.Value.ToLongDateString() });
                 }
                 else
                 {
-                    _logger.LogInformation(
+                    Action<string, object[]> log = _license.ISVFeature ? _logger.LogTrace : _logger.LogInformation;
+                    log.Invoke(
                         "You have a valid license key for Duende IdentityServer {edition} edition for use at {licenseCompany}.",
-                        _license.Edition, _license.CompanyName);
+                        new object[] { _license.Edition, _license.CompanyName });
                 }
             }
         }
@@ -148,75 +143,6 @@ namespace Duende.Bff.Endpoints
             }
 
             return null;
-        }
-
-        internal class License
-        {
-            public string CompanyName { get; set; }
-            public string ContactInfo { get; set; }
-
-            public DateTime? Expiration { get; set; }
-
-            public LicenseEdition Edition { get; set; }
-
-            internal bool IsEnterprise => Edition == LicenseEdition.Enterprise;
-            internal bool IsBusiness => Edition == LicenseEdition.Business;
-            internal bool IsStarter => Edition == LicenseEdition.Starter;
-            internal bool IsCommunity => Edition == LicenseEdition.Community;
-
-
-            public bool BFF { get; set; }
-            public bool ISV { get; set; }
-
-            public enum LicenseEdition
-            {
-                Enterprise,
-                Business,
-                Starter,
-                Community
-            }
-            
-            // for testing
-            internal License(params Claim[] claims)
-                : this(new ClaimsPrincipal(new ClaimsIdentity(claims)))
-            {
-            }
-
-            public License(ClaimsPrincipal claims)
-            {
-                CompanyName = claims.FindFirst("company_name")?.Value;
-                ContactInfo = claims.FindFirst("contact_info")?.Value;
-
-                if (Int64.TryParse(claims.FindFirst("exp")?.Value, out var exp))
-                {
-                    var expDate = DateTimeOffset.FromUnixTimeSeconds(exp);
-                    Expiration = expDate.UtcDateTime;
-                }
-
-                var edition = claims.FindFirst("edition")?.Value;
-                if (!Enum.TryParse<License.LicenseEdition>(edition, true, out var editionValue))
-                {
-                    throw new Exception($"Invalid edition in licence: '{edition}'");
-                }
-
-                Edition = editionValue;
-                ISV = claims.HasClaim("feature", "isv");
-                BFF = claims.HasClaim("feature", "bff");
-
-                if (ISV && IsCommunity)
-                {
-                    throw new Exception("Invalid License: ISV is not valid for community edition.");
-                }
-            }
-
-            public override string ToString()
-            {
-#if NET6_0_OR_GREATER
-                return JsonSerializer.Serialize(this, new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull });
-#else
-                return JsonSerializer.Serialize(this, new JsonSerializerOptions { IgnoreNullValues = true });
-#endif
-            }
         }
     }
 }
