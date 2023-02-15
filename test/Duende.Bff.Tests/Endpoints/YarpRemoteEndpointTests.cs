af56dd24c632a86c97d3db2039008bd87ec85c43
Dominick Baier <dbaier@leastprivilege.com>
2023-02-15 11:47:38 +0100

    Added tests for YARP extensions

---
+++ test/Duende.Bff.Tests/Endpoints/YarpRemoteEndpointTests.cs
@@ -0,0 +1,193 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using Duende.Bff.Tests.TestHosts;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Duende.Bff.Tests.TestFramework;
+using Xunit;
+
+namespace Duende.Bff.Tests.Endpoints
+{
+    public class YarpRemoteEndpointTests : YarpBffIntegrationTestBase
+    {
+        [Fact]
+        public async Task anonymous_call_with_no_csrf_header_to_no_token_requirement_no_csrf_route_should_succeed()
+        {
+            var req = new HttpRequestMessage(HttpMethod.Get, BffHost.Url("/api_anon_no_csrf/test"));
+            var response = await BffHost.BrowserClient.SendAsync(req);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+        
+        [Fact]
+        public async Task anonymous_call_with_no_csrf_header_to_csrf_route_should_fail()
+        {
+            var req = new HttpRequestMessage(HttpMethod.Get, BffHost.Url("/api_anon/test"));
+            var response = await BffHost.BrowserClient.SendAsync(req);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+        
+        [Fact]
+        public async Task anonymous_call_to_no_token_requirement_route_should_succeed()
+        {
+            var req = new HttpRequestMessage(HttpMethod.Get, BffHost.Url("/api_anon/test"));
+            req.Headers.Add("x-csrf", "1");
+            var response = await BffHost.BrowserClient.SendAsync(req);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+        
+        [Fact]
+        public async Task anonymous_call_to_user_token_requirement_route_should_fail()
+        {
+            var req = new HttpRequestMessage(HttpMethod.Get, BffHost.Url("/api_user/test"));
+            req.Headers.Add("x-csrf", "1");
+            var response = await BffHost.BrowserClient.SendAsync(req);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task authenticated_GET_should_forward_user_to_api()
+        {
+            await BffHost.BffLoginAsync("alice");
+        
+            var req = new HttpRequestMessage(HttpMethod.Get, BffHost.Url("/api_user/test"));
+            req.Headers.Add("x-csrf", "1");
+            var response = await BffHost.BrowserClient.SendAsync(req);
+        
+            response.IsSuccessStatusCode.Should().BeTrue();
+            response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
+            var json = await response.Content.ReadAsStringAsync();
+            var apiResult = JsonSerializer.Deserialize<ApiResponse>(json);
+            apiResult.Method.Should().Be("GET");
+            apiResult.Path.Should().Be("/api_user/test");
+            apiResult.Sub.Should().Be("alice");
+            apiResult.ClientId.Should().Be("spa");
+        }
+        
+        [Fact]
+        public async Task authenticated_PUT_should_forward_user_to_api()
+        {
+            await BffHost.BffLoginAsync("alice");
+        
+            var req = new HttpRequestMessage(HttpMethod.Put, BffHost.Url("/api_user/test"));
+            req.Headers.Add("x-csrf", "1");
+            var response = await BffHost.BrowserClient.SendAsync(req);
+        
+            response.IsSuccessStatusCode.Should().BeTrue();
+            response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
+            var json = await response.Content.ReadAsStringAsync();
+            var apiResult = JsonSerializer.Deserialize<ApiResponse>(json);
+            apiResult.Method.Should().Be("PUT");
+            apiResult.Path.Should().Be("/api_user/test");
+            apiResult.Sub.Should().Be("alice");
+            apiResult.ClientId.Should().Be("spa");
+        }
+        
+        [Fact]
+        public async Task authenticated_POST_should_forward_user_to_api()
+        {
+            await BffHost.BffLoginAsync("alice");
+        
+            var req = new HttpRequestMessage(HttpMethod.Post, BffHost.Url("/api_user/test"));
+            req.Headers.Add("x-csrf", "1");
+            var response = await BffHost.BrowserClient.SendAsync(req);
+        
+            response.IsSuccessStatusCode.Should().BeTrue();
+            response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
+            var json = await response.Content.ReadAsStringAsync();
+            var apiResult = JsonSerializer.Deserialize<ApiResponse>(json);
+            apiResult.Method.Should().Be("POST");
+            apiResult.Path.Should().Be("/api_user/test");
+            apiResult.Sub.Should().Be("alice");
+            apiResult.ClientId.Should().Be("spa");
+        }
+        
+        [Fact]
+        public async Task call_to_client_token_route_should_forward_client_token_to_api()
+        {
+            await BffHost.BffLoginAsync("alice");
+        
+            var req = new HttpRequestMessage(HttpMethod.Get, BffHost.Url("/api_client/test"));
+            req.Headers.Add("x-csrf", "1");
+            var response = await BffHost.BrowserClient.SendAsync(req);
+        
+            response.IsSuccessStatusCode.Should().BeTrue();
+            response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
+            var json = await response.Content.ReadAsStringAsync();
+            var apiResult = JsonSerializer.Deserialize<ApiResponse>(json);
+            apiResult.Method.Should().Be("GET");
+            apiResult.Path.Should().Be("/api_client/test");
+            apiResult.Sub.Should().BeNull();
+            apiResult.ClientId.Should().Be("spa");
+        }
+        
+        [Fact]
+        public async Task call_to_user_or_client_token_route_should_forward_user_or_client_token_to_api()
+        {
+            {
+                var req = new HttpRequestMessage(HttpMethod.Get, BffHost.Url("/api_user_or_client/test"));
+                req.Headers.Add("x-csrf", "1");
+                var response = await BffHost.BrowserClient.SendAsync(req);
+        
+                response.IsSuccessStatusCode.Should().BeTrue();
+                response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
+                var json = await response.Content.ReadAsStringAsync();
+                var apiResult = JsonSerializer.Deserialize<ApiResponse>(json);
+                apiResult.Method.Should().Be("GET");
+                apiResult.Path.Should().Be("/api_user_or_client/test");
+                apiResult.Sub.Should().BeNull();
+                apiResult.ClientId.Should().Be("spa");
+            }
+        
+            {
+                await BffHost.BffLoginAsync("alice");
+        
+                var req = new HttpRequestMessage(HttpMethod.Get, BffHost.Url("/api_user_or_client/test"));
+                req.Headers.Add("x-csrf", "1");
+                var response = await BffHost.BrowserClient.SendAsync(req);
+        
+                response.IsSuccessStatusCode.Should().BeTrue();
+                response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
+                var json = await response.Content.ReadAsStringAsync();
+                var apiResult = JsonSerializer.Deserialize<ApiResponse>(json);
+                apiResult.Method.Should().Be("GET");
+                apiResult.Path.Should().Be("/api_user_or_client/test");
+                apiResult.Sub.Should().Be("alice");
+                apiResult.ClientId.Should().Be("spa");
+            }
+        }
+        
+        [Fact]
+        public async Task response_status_401_from_remote_endpoint_should_return_401_from_bff()
+        {
+            await BffHost.BffLoginAsync("alice");
+            ApiHost.ApiStatusCodeToReturn = 401;
+        
+            var req = new HttpRequestMessage(HttpMethod.Get, BffHost.Url("/api_user/test"));
+            req.Headers.Add("x-csrf", "1");
+            var response = await BffHost.BrowserClient.SendAsync(req);
+        
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+        
+        [Fact]
+        public async Task response_status_403_from_remote_endpoint_should_return_403_from_bff()
+        {
+            await BffHost.BffLoginAsync("alice");
+            ApiHost.ApiStatusCodeToReturn = 403;
+        
+            var req = new HttpRequestMessage(HttpMethod.Get, BffHost.Url("/api_user/test"));
+            req.Headers.Add("x-csrf", "1");
+            var response = await BffHost.BrowserClient.SendAsync(req);
+        
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+    }
+}
---
+++ test/Duende.Bff.Tests/TestHosts/YarpBffIntegrationTestBase.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Duende.Bff.Tests.TestHosts
+{
+    public class YarpBffIntegrationTestBase
+    {
+        protected readonly IdentityServerHost IdentityServerHost;
+        protected ApiHost ApiHost;
+        protected YarpBffHost BffHost;
+        protected BffHostUsingResourceNamedTokens BffHostWithNamedTokens;
+
+        public YarpBffIntegrationTestBase()
+        {
+            IdentityServerHost = new IdentityServerHost();
+            
+            IdentityServerHost.Clients.Add(new Client
+            {
+                ClientId = "spa",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RedirectUris = { "https://app/signin-oidc" },
+                PostLogoutRedirectUris = { "https://app/signout-callback-oidc" },
+                BackChannelLogoutUri = "https://app/bff/backchannel",
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "scope1" }
+            });
+            
+            
+            IdentityServerHost.OnConfigureServices += services => {
+                services.AddTransient<IBackChannelLogoutHttpClient>(provider => 
+                    new DefaultBackChannelLogoutHttpClient(
+                        BffHost.HttpClient, 
+                        provider.GetRequiredService<ILoggerFactory>(), 
+                        provider.GetRequiredService<ICancellationTokenProvider>()));
+            };
+            
+            IdentityServerHost.InitializeAsync().Wait();
+
+            ApiHost = new ApiHost(IdentityServerHost, "scope1");
+            ApiHost.InitializeAsync().Wait();
+
+            BffHost = new YarpBffHost(IdentityServerHost, ApiHost, "spa");
+            BffHost.InitializeAsync().Wait();
+
+            BffHostWithNamedTokens = new BffHostUsingResourceNamedTokens(IdentityServerHost, ApiHost, "spa");
+            BffHostWithNamedTokens.InitializeAsync().Wait();
+        }
+
+        public async Task Login(string sub)
+        {
+            await IdentityServerHost.IssueSessionCookieAsync(new Claim("sub", sub));
+        }
+    }
+}
