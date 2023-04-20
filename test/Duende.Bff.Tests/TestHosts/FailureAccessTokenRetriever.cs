158706ced02f592108cc75dc533e298f60594cc7
Joe DeCock <josephdecock@gmail.com>
2023-04-20 12:28:24 -0500

    Add automated tests of access token retrieval

---
+++ test/Duende.Bff.Tests/TestHosts/FailureAccessTokenRetriever.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using System.Threading.Tasks;
+
+namespace Duende.Bff.Tests.TestHosts;
+
+public class FailureAccessTokenRetriever : IAccessTokenRetriever
+{
+    public Task<AccessTokenResult> GetAccessToken(AccessTokenRetrievalContext context)
+    {
+        return Task.FromResult(new AccessTokenResult
+        {
+            IsError = true,
+        });
+    }
+}
