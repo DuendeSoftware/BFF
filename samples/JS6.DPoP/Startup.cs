7805d4e62e7fb0e24141626adffbe5570cec992a
Joe DeCock <josephdecock@gmail.com>
2023-06-13 11:58:10 -0500

    Add dpop sample

---
+++ samples/JS6.DPoP/Startup.cs
@@ -0,0 +1,128 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text.Json;
+using Duende.Bff;
+using Duende.Bff.Yarp;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+
+namespace JS6.DPoP;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add BFF services to DI - also add server-side session management
+        services.AddBff(options =>
+        {
+            var rsaKey = new RsaSecurityKey(RSA.Create(2048));
+            var jwkKey = JsonWebKeyConverter.ConvertFromSecurityKey(rsaKey);
+            jwkKey.Alg = "PS256";
+            var jwk = JsonSerializer.Serialize(jwkKey);
+            options.DPoPJsonWebKey = jwk;
+        })
+        .AddRemoteApis()
+        .AddServerSideSessions();
+
+        // local APIs
+        services.AddControllers();
+
+        // cookie options
+        services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "cookie";
+                options.DefaultChallengeScheme = "oidc";
+                options.DefaultSignOutScheme = "oidc";
+            })
+            .AddCookie("cookie", options =>
+            {
+                // set session lifetime
+                options.ExpireTimeSpan = TimeSpan.FromHours(8);
+
+                // sliding or absolute
+                options.SlidingExpiration = false;
+
+                // host prefixed cookie name
+                options.Cookie.Name = "__Host-spa6-dpop";
+
+                // strict SameSite handling
+                options.Cookie.SameSite = SameSiteMode.Strict;
+            })
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.Authority = "https://localhost:5001";
+
+                // confidential client using code flow + PKCE
+                options.ClientId = "spa";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code";
+                options.ResponseMode = "query";
+
+                options.MapInboundClaims = false;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.SaveTokens = true;
+
+                // request scopes + refresh tokens
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("api");
+                options.Scope.Add("offline_access");
+            });
+        services.AddSingleton<ImpersonationAccessTokenRetriever>();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseSerilogRequestLogging();
+        app.UseDeveloperExceptionPage();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.UseAuthentication();
+        app.UseRouting();
+
+        // adds antiforgery protection for local APIs
+        app.UseBff();
+
+        // adds authorization for local and remote API endpoints
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            // local APIs
+            endpoints.MapControllers()
+                .RequireAuthorization()
+                .AsBffApiEndpoint();
+
+            // login, logout, user, backchannel logout...
+            endpoints.MapBffManagementEndpoints();
+
+            // On this path, we use a client credentials token
+            endpoints.MapRemoteBffApiEndpoint("/api/client-credentials-token", "https://localhost:5010")
+                .RequireAccessToken(TokenType.Client);
+
+            // On this path, we perform token exchange to impersonate a different user
+            // before making the api request
+            endpoints.MapRemoteBffApiEndpoint("/api/impersonation", "https://localhost:5010")
+                .RequireAccessToken(TokenType.UserOrClient)
+                .WithAccessTokenRetriever<ImpersonationAccessTokenRetriever>();
+
+            // proxy endpoint for cross-site APIs
+            // all calls to /api/* will be forwarded to the remote API
+            // user or client access token will be attached in API call
+            // user access token will be managed automatically using the refresh token
+            endpoints.MapRemoteBffApiEndpoint("/api", "https://localhost:5010")
+                .RequireAccessToken(TokenType.UserOrClient);
+
+        });
+    }
+}
---
+++ samples/JS6.DPoP/wwwroot/app.js
@@ -0,0 +1,202 @@
+﻿const loginUrl = "/bff/login";
+const silentLoginUrl = "/bff/silent-login";
+const userUrl = "/bff/user";
+const localApiUrl = "/local";
+const remoteApiUrl = "/api";
+let logoutUrl = "/bff/logout";
+
+async function onLoad() {
+    var req = new Request(userUrl, {
+        headers: new Headers({
+            'X-CSRF': '1'
+        })
+    })
+
+    try {
+        var resp = await fetch(req);
+        if (resp.ok) {
+
+            let claims = await resp.json();
+            showUser(claims);
+
+            if (claims) {
+                log("user logged in");
+
+                let logoutUrlClaim = claims.find(claim => claim.type === 'bff:logout_url');
+                if (logoutUrlClaim) {
+                    logoutUrl = logoutUrlClaim.value;
+                }
+            }
+            else {
+                log("user not logged in");
+            }
+        } else if (resp.status === 401) {
+            log("user not logged in");
+
+            // if we've detected that the user is no already logged in, we can attempt a silent login
+            // this will trigger a normal OIDC request in an iframe using prompt=none.
+            // if the user is already logged into IdentityServer, then the result will establish a session in the BFF.
+            // this whole process avoids redirecting the top window without knowing if the user is logged in or not.
+            var silentLoginResult = await silentLogin();
+
+            // the result is a boolean letting us know if the user has been logged in silently
+            log("silent login result: " + silentLoginResult);
+
+            if (silentLoginResult) {
+                // if we now have a user logged in silently, then reload this window
+                window.location.reload();
+            }
+        }
+    }
+    catch (e) {
+        log("error checking user status");
+    }
+}
+
+onLoad();
+
+function login() {
+    window.location = loginUrl;
+}
+
+function logout() {
+    window.location = logoutUrl;
+}
+
+async function callLocalApi() {
+    var req = new Request(localApiUrl, {
+        headers: new Headers({
+            'X-CSRF': '1'
+        })
+    })
+    var resp = await fetch(req);
+
+    log("API Result: " + resp.status);
+    if (resp.ok) {
+        showApi(await resp.json());
+    }
+}
+
+async function callCrossApi() {
+    var req = new Request(remoteApiUrl + "/foo", {
+        headers: new Headers({
+            'X-CSRF': '1'
+        })
+    })
+    var resp = await fetch(req);
+
+    log("API Result: " + resp.status);
+    if (resp.ok) {
+        showApi(await resp.json());
+    }
+}
+
+async function callImpersonationApi() {
+    var req = new Request(remoteApiUrl + "/impersonation", {
+        headers: new Headers({
+            'X-CSRF': '1'
+        })
+    })
+    var resp = await fetch(req);
+
+    log("API Result: " + resp.status);
+    if (resp.ok) {
+        showApi(await resp.json());
+    }
+}
+
+async function callClientCredentialsApi() {
+    var req = new Request(remoteApiUrl + "/client-credentials-token", {
+        headers: new Headers({
+            'X-CSRF': '1'
+        })
+    })
+    var resp = await fetch(req);
+
+    log("API Result: " + resp.status);
+    if (resp.ok) {
+        showApi(await resp.json());
+    }
+}
+
+document.querySelector(".login").addEventListener("click", login, false);
+document.querySelector(".call_cross").addEventListener("click", callCrossApi, false);
+document.querySelector(".call_impersonation").addEventListener("click", callImpersonationApi, false);
+document.querySelector(".call_client_credentials").addEventListener("click", callClientCredentialsApi, false);
+document.querySelector(".call_local").addEventListener("click", callLocalApi, false);
+document.querySelector(".logout").addEventListener("click", logout, false);
+
+
+function showApi() {
+    document.getElementById('api-result').innerText = '';
+
+    Array.prototype.forEach.call(arguments, function (msg) {
+        if (msg instanceof Error) {
+            msg = "Error: " + msg.message;
+        } else if (typeof msg !== 'string') {
+            msg = JSON.stringify(msg, null, 2);
+        }
+        document.getElementById('api-result').innerText += msg + '\r\n';
+    });
+}
+
+function showUser() {
+    document.getElementById('user').innerText = '';
+
+    Array.prototype.forEach.call(arguments, function (msg) {
+        if (msg instanceof Error) {
+            msg = "Error: " + msg.message;
+        } else if (typeof msg !== 'string') {
+            msg = JSON.stringify(msg, null, 2);
+        }
+        document.getElementById('user').innerText += msg + '\r\n';
+    });
+}
+
+function log() {
+    document.getElementById('response').innerText = '';
+
+    Array.prototype.forEach.call(arguments, function (msg) {
+        if (msg instanceof Error) {
+            msg = "Error: " + msg.message;
+        } else if (typeof msg !== 'string') {
+            msg = JSON.stringify(msg, null, 2);
+        }
+        document.getElementById('response').innerText += msg + '\r\n';
+    });
+}
+
+
+// this will trigger the silent login and return a promise that resolves to true or false.
+function silentLogin(iframeSelector) {
+    iframeSelector = iframeSelector || "#bff-silent-login";
+    const timeout = 5000;
+
+    return new Promise((resolve, reject) => {
+        function onMessage(e) {
+            // look for messages sent from the BFF iframe
+            if (e.data && e.data['source'] === 'bff-silent-login') {
+                window.removeEventListener("message", onMessage);
+                // send along the boolean result
+                resolve(e.data.isLoggedIn);
+            }
+        };
+
+        // listen for the iframe response to notify its parent (i.e. this window).
+        window.addEventListener("message", onMessage);
+
+        // we're setting up a time to handle scenarios when the iframe doesn't return immediaetly (despite prompt=none).
+        // this likely means the iframe is showing the error page at IdentityServer (typically due to client misconfiguration).
+        window.setTimeout(() => {
+            window.removeEventListener("message", onMessage);
+
+            // we can either just treat this like a "not logged in"
+            resolve(false);
+            // or we can trigger an error, so someone can look into the reason why
+            // reject(new Error("timed_out")); 
+        }, timeout);
+
+        // send the iframe to the silent login endpoint to kick off the workflow
+        document.querySelector(iframeSelector).src = silentLoginUrl;
+    });
+}
