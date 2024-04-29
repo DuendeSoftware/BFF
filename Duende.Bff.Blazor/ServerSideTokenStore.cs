583acade9720c9dcdabadde8414ed44a4eeed537
Joe DeCock <josephdecock@gmail.com>
2024-04-29 12:40:22 -0500

    Improve blazor ServerSide tokens to respect more optional parameters
    Distinct tokens can now be stored per challenge scheme and per resource, using logic from Duende.AccessTokenManagement

--- Duende.Bff.Blazor/ServerSideTokenStore.cs
+++ Duende.Bff.Blazor/ServerSideTokenStore.cs
@@ -1,16 +1,11 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-using System.Collections.Concurrent;
-using System.Globalization;
-using System.Net.Sockets;
 using System.Security.Claims;
 using Duende.AccessTokenManagement.OpenIdConnect;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace Duende.Bff.Blazor;
 
@@ -20,39 +15,18 @@ namespace Duende.Bff.Blazor;
 /// </summary>
 
 public class ServerSideTokenStore(
+    IStoreTokensInAuthenticationProperties tokensInAuthProperties,
     IUserSessionStore sessionStore, 
     IDataProtectionProvider dataProtectionProvider,
     ILogger<ServerSideTokenStore> logger) : IUserTokenStore
 {
     private readonly IDataProtector protector = dataProtectionProvider.CreateProtector("Duende.Bff.ServerSideTicketStore"); // TODO - Replace with constant
-    // TODO - Respect UserTokenRequestParameters
     public async Task<UserToken> GetTokenAsync(ClaimsPrincipal user, UserTokenRequestParameters? parameters = null)
     {
         var session = await GetSession(user);
-        var ticket = session.Deserialize(protector, logger) ?? throw new InvalidOperationException("TODO");
+        var ticket = session.Deserialize(protector, logger) ?? throw new InvalidOperationException("Failed to deserialize authentication ticket from session");
 
-        var expiration = ticket.Properties.GetTokenValue("expires_at");
-        DateTimeOffset dtExpires = DateTimeOffset.MaxValue;
-        if (expiration != null)
-        {
-            dtExpires = DateTimeOffset.Parse(expiration, CultureInfo.InvariantCulture);
-        }
-
-        // TODO - use scheme and resource in name, optionally. Copy logic from AccessTokenManagement
-        // TODO - consider making that logic reusable from ATM
-        var tokens = new UserToken
-        {
-            AccessToken = ticket.Properties.GetTokenValue(OpenIdConnectParameterNames.AccessToken),
-            RefreshToken = ticket.Properties.GetTokenValue(OpenIdConnectParameterNames.RefreshToken),
-            AccessTokenType = ticket.Properties.GetTokenValue(OpenIdConnectParameterNames.TokenType),
-            DPoPJsonWebKey = ticket.Properties.GetTokenValue("dpop_proof_key"),
-            Expiration = dtExpires
-        };
-
-        logger.LogTrace("Retrieved server side tokens with expiration {expiration}", dtExpires);
-
-
-        return tokens;
+        return tokensInAuthProperties.GetUserToken(ticket.Properties, parameters);
     }
 
     private async Task<UserSession> GetSession(ClaimsPrincipal user)
@@ -66,45 +40,32 @@ public class ServerSideTokenStore(
             SessionId = sid
         });
 
-        // TODO - should we throw or return a UserToken object with the error property set?
         if (sessions.Count == 0) throw new InvalidOperationException("No ticket found");
         if (sessions.Count > 1) throw new InvalidOperationException("Multiple tickets found");
 
         return sessions.First();
     }
 
-    // TODO - use scheme and resource in name, optionally. Copy logic from AccessTokenManagement
-    // TODO - consider making that logic reusable from ATM
     public async Task StoreTokenAsync(ClaimsPrincipal user, UserToken token, UserTokenRequestParameters? parameters = null)
     {
         await UpdateTicket(user, ticket =>
         {
-            ticket.Properties.Items[".Token." +OpenIdConnectParameterNames.AccessToken] = token.AccessToken;
-            ticket.Properties.Items[".Token." + OpenIdConnectParameterNames.TokenType] = token.AccessTokenType;
-            ticket.Properties.Items[".Token." + OpenIdConnectParameterNames.RefreshToken] = token.RefreshToken;
-            ticket.Properties.Items[".Token.dpop_proof_key"] = token.DPoPJsonWebKey;
-            ticket.Properties.Items[".Token.expires_at"] = token.Expiration.ToString("o", CultureInfo.InvariantCulture);
+            tokensInAuthProperties.SetUserToken(token, ticket.Properties, "", parameters);
         });
     }
 
-    // TODO - use scheme and resource in name, optionally. Copy logic from AccessTokenManagement
-    // TODO - consider making that logic reusable from ATM
     public async Task ClearTokenAsync(ClaimsPrincipal user, UserTokenRequestParameters? parameters = null)
     {
         await UpdateTicket(user, ticket =>
         {
-            ticket.Properties.Items.Remove(OpenIdConnectParameterNames.AccessToken);
-            ticket.Properties.Items.Remove(OpenIdConnectParameterNames.TokenType);
-            ticket.Properties.Items.Remove(OpenIdConnectParameterNames.RefreshToken);
-            ticket.Properties.Items.Remove("dpop_proof_key");
-            ticket.Properties.Items.Remove("expires_at");
+            tokensInAuthProperties.RemoveUserToken(ticket.Properties, parameters);
         });
     }
 
     public async Task UpdateTicket(ClaimsPrincipal user, Action<AuthenticationTicket> updateAction)
     {
         var session = await GetSession(user);
-        var ticket = session.Deserialize(protector, logger) ?? throw new InvalidOperationException("TODO");
+        var ticket = session.Deserialize(protector, logger) ?? throw new InvalidOperationException("Failed to deserialize authentication ticket from session");
 
         updateAction(ticket);
 
